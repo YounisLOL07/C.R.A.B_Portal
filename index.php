fb4a10bb83532bbda136cd46276a36fdbe942af4
YounisLOL07 <Khelyounis123@gmail.com>
2025-03-25 21:33:55 +0100

    Adding and styling navbar

--- index.php
+++ index.php
@@ -3,10 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>C.R.A.B Portal</title>
 </head>
 <body>
     <div class="container">
+        <div class="navbar">
+            <a href="">Home</a>
+            <a href="">Highscore</a>
+            <a href="">FAQ</a>
+        </div>
+        
         <h1>This is the Crab Portal!</h1>
         <h2>Here are all the games!</h2>
     </div>
@@ -14,6 +21,6 @@
 </html>
 
 <?php
-    
+
 
 ?>
