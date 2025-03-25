da90af8b2ce4ae525348bf4783484892a209ef09
YounisLOL07 <Khelyounis123@gmail.com>
2025-03-25 21:19:33 +0100

    Added new files with content

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>C.R.A.B Portal</title>
+</head>
+<body>
+    <div class="container">
+        <h1>This is the Crab Portal!</h1>
+        <h2>Here are all the games!</h2>
+    </div>
+</body>
+</html>
+
+<?php
+    
+
+?>
