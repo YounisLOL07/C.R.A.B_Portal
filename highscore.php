5477b431a6c1d70ea5eb5b48a7469fcbe6ee35a8
YounisLOL07 <Khelyounis123@gmail.com>
2025-06-11 23:36:32 +0200

    revert back to normal

--- highscore.php
+++ highscore.php
@@ -21,7 +21,6 @@ $highscores = $stmt->fetchAll();
 <body>
     <div class="container">
         <div class="navbar">
-            //navbar
             <a href="index.php">Home</a>
             <?php if (isset($_SESSION['username'])): ?>
                 <span class="logged-in">Hello, <?php echo htmlspecialchars($_SESSION['username']); ?></span>
